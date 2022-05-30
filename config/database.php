fb6e7918e127fcf017315453b8ecd7f5d73aa223
aviran <avirandabush@gmail.com>
2022-05-31 00:32:02 +0300

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'aviran');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'php');
+
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if($conn->connect_error)
+    {
+        die('Connection Failed ' . $conn->connect_error);
+    }
