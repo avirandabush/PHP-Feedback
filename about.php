78eca932c07ddb65391d4987b491d86ba838ca0d
aviran <avirandabush@gmail.com>
2022-05-31 00:31:45 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php include 'inc/header.php'; ?>
+
+<main>
+  <div class="container d-flex flex-column align-items-center">
+    <h2>About</h2>
+
+    <p class="text-center">A Computer Science student & a fullstack developer</p>
+    <p>you can learn more about me at the link below</p>
+    <a href="http://avirandabush.co.il">Portfolio Website</a>
+    </div>
+</main>
+
+<?php include 'inc/foother.php'; ?>
